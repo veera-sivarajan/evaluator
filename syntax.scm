19eb86b295c06a65747dad6eabbad494f5ee79c7
Veera <sveera.2001@gmail.com>
2021-06-05 15:44:48 -0400

    Seperate define into func and var
    Two different kinds of expressions can be defined using define.
    So I have it into two different forms: (var var-name exp) and
    (func (func-name params) (exps)). This commit adds extractors
    for both the forms.

--- syntax.scm
+++ syntax.scm
@@ -3,7 +3,7 @@
 
 (define (self-eval? exp) (or (number? exp) (string? exp))) 
 
-(define (var-name? exp) (symbol? exp)) ;; variable name == symbol
+;; (define (var-name? exp) (symbol? exp)) ;; variable name == symbol
 
 (define (oper=? exp name) (and (pair? exp) (eq? (car exp) name))) 
 
@@ -25,21 +25,29 @@
       (car (cdr (cdr exp)))
       (error "Wrong expression for assign-value:" exp))) 
 
-(define (def? exp)
-  (oper=? exp 'def)) ; def instead of define 
+(define (func? exp)
+  (oper=? exp 'func)) ; func instead of define 
 
-(define (def-name exp)
-  (if (def? exp)
-      (if (var-name? (car (cdr exp))) 
-          (car (cdr exp)) ; (def x 2) -> x
-          (car (car (cdr exp)))) ; (def (square n) (* n n)) -> square
-      (error "Wrong expression for def-name:" exp))) 
+(define (func-name exp)
+  (if (func? exp)
+      (car (car (cdr exp)))) ; (func (square n) (* n n)) -> square
+      (error "Wrong expression for def-name:" exp)) 
 
-(define (def-value exp)
-  (define (formal-params exp) (cdr (car (cdr exp)))) 
-  (if (def? exp)
-      (if (var-name? (car (cdr exp)))
-          (car (cdr (cdr exp))) ; (def x (+ 1 2)) -> (+ 1 2)
-          (make-lambda (formal-params exp) (cdr (cdr exp))))
-      (error "Wrong expression for def-value:" exp))) 
-      
+(define (func-value exp)
+  (let ((f-params (cdr (car (cdr exp))))
+        (body (cdr (cdr exp)))) ; func body can be multiple expressions
+        (if (func? exp) ;(func (fact n) (if (= n 0) 1 (* n (fact (- n 1))))
+            (make-lambda f-params body) 
+            (error "Wrong expression for func-value:" exp)))) 
+
+(define (var? exp) (oper=? exp 'var))
+
+(define (var-name exp)
+  (if (var? exp) ; (var x (+ 1 (+ 2 3)))
+      (car (cdr exp))
+      (error "Wrong expression for var-name:" exp))) 
+
+(define (var-value exp)
+  (if (var? exp)
+      (car (cdr (cdr exp)))
+      (error "Wrong expression for var-value:" exp))) 
