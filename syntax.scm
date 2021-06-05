5351a423838f3538ad2f4d3b61a9313d98c0a0f0
Veera <sveera.2001@gmail.com>
2021-06-05 17:38:48 -0400

    Add extractors for conditionals & check if name for name extractors
    This commit adds checks to ensure that all name extractors return
    a name rather than a expression. Also, added extractors for if
    conditionals.

--- syntax.scm
+++ syntax.scm
@@ -3,7 +3,8 @@
 
 (define (self-eval? exp) (or (number? exp) (string? exp))) 
 
-;; (define (var-name? exp) (symbol? exp)) ;; variable name == symbol
+;; (define (name? exp) (symbol? exp)) ;; variable name == symbol
+(define name? symbol?) 
 
 (define (oper=? exp name) (and (pair? exp) (eq? (car exp) name))) 
 
@@ -15,7 +16,7 @@
 
 ;; (set! x 2) -> x
 (define (assign-var exp)
-  (if (assign? exp)
+  (if (and (assign? exp) (name? (car (cdr exp))))
       (car (cdr exp))
       (error "Wrong expression for assign-var:" exp))) 
   
@@ -29,7 +30,7 @@
   (oper=? exp 'func)) ; func instead of define 
 
 (define (func-name exp)
-  (if (func? exp)
+  (if (and (func? exp) (name? (car (car (cdr exp)))))
       (car (car (cdr exp)))) ; (func (square n) (* n n)) -> square
       (error "Wrong expression for def-name:" exp)) 
 
@@ -43,7 +44,7 @@
 (define (var? exp) (oper=? exp 'var))
 
 (define (var-name exp)
-  (if (var? exp) ; (var x (+ 1 (+ 2 3)))
+  (if (and (var? exp) (name? (car (cdr exp)))) ; (var x (+ 1 (+ 2 3))) -> x
       (car (cdr exp))
       (error "Wrong expression for var-name:" exp))) 
 
@@ -67,4 +68,24 @@
 (define (build-make-proc params body)
   (cons 'make-proc (cons params body))) 
 
+(define (if? exp) (oper=? exp 'if))
 
+(define (if-pred exp)
+  (if (if? exp) 
+      (car (cdr exp))
+      (error "Wrong expression for if-pred:" exp)))
+
+(define (if-conseq exp)
+  (if (if? exp) ; (if (null? lis) '() (cons 1 2))
+      (car (cdr (cdr exp)))
+      (error "Wrong expression for if-conseq:" exp)))
+
+(define (if-altern exp) ; proc for else clause
+  (if (if? exp)
+      (if (not (null? (cdr (cdr (cdr exp)))))
+          (car (cdr (cdr (cdr exp))))
+          'false)
+      (error "Wrong expression for if-altern:" exp))) 
+      
+(define (build-if pred conseq altern)
+  (list 'if pred conseq altern)) 
