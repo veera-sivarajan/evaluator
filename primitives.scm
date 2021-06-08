d1a61765508f035d9adecca6dc227f40cb7e4d0a
Veera <sveera.2001@gmail.com>
2021-06-08 13:51:56 -0400

    Add more primitives for list manipulation
    This commit adds procedures for length, reverse and appending two
    lists into one using foldl and foldr implemented previously. It
    also adds my implementation of list-ref with better error
    checking.

--- primitives.scm
+++ primitives.scm
@@ -17,3 +17,20 @@
         (cons first result)
         result))
   (foldr filter-helper '() lis)) 
+
+(define (v-reverse lis)
+  (foldl (lambda (init first) (cons first init)) '() lis)) 
+
+(define (v-length lis)
+  (foldl (lambda (init first) (+ init 1)) 0 lis)) 
+
+(define (v-append list-a list-b)
+  (foldr (lambda (first init) (cons first init)) list-b list-a)) 
+
+(define (v-list-ref lis index)
+  (if (and (< index (v-length lis)) (>= index 0))
+      (if (= index 0)
+          (car lis)
+          (v-list-ref (cdr lis) (- index 1)))
+      (error "Index out of range" index))) 
+
