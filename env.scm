e46877bf912a885782c39ef8f19d39d3caefc747
Veera <sveera.2001@gmail.com>
2021-06-10 23:50:39 -0400

    Add procedure to define variable in frame
    The procedure added here is the exact copy from the book. I
    should try to implement it in a better way tomorrow. Also, fix
    a couple of bugs.

--- env.scm
+++ env.scm
@@ -3,9 +3,9 @@
 ;; Current environment is the car of environment (first ele in list)
 ;; Enclosing environment is cdr of environment
 
-(define (prev-envs envs) (cdr env))
+(define (prev-envs envs) (cdr envs))
 
-(define (curr-env envs) (car env))
+(define (curr-env envs) (car envs)) 
 
 (define empty-env '())
 
@@ -36,7 +36,6 @@
           (scan (frame-vars frame) (frame-vals vals)))))
   (env-loop env)) 
 
-
 (define (set-var-value! var val env)
   (define (env-loop env)
     (define (scan vars vals)
@@ -45,7 +44,15 @@
             (else (scan (cdr vars) (cdr vals)))))
     (if (eq? env empty-env)
         (error "Unbound variable -- SET" var)
-        (let ((frame (curr-frame env)))
+        (let ((frame (curr-env env)))
           (scan (frame-vars frame) (frame-vals frame)))))
   (env-loop env)) 
-              
+
+(define (define-variable! var val env)
+  (let ((frame (curr-frame env)))
+    (define (scan vars vals)
+      (cond ((null? vars) (add-binding-to-frame! var val frame))
+            ((eq? var (car vars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-vars frame) (frame-vals frame)))) 
+    
