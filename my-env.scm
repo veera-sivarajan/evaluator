7258d2215f3b4881a5c62306b46f01c4784e0afc
Veera <sveera.2001@gmail.com>
2021-06-12 00:14:37 -0400

    Add lookup variable for new environment
    This implementation needs a lot of bug fixing to make it work
    correctly. The general structure of variable lookup seems to be
    right. The bugs arise from accessing the wrong parts of the
    bindings in a frame. For fixing, I should keep in mind that the
    initial environment is an extension of empty environment.

--- my-env.scm
+++ my-env.scm
@@ -10,16 +10,43 @@
                   (new-frame (cdr vars) (cdr vals)))))
 
 (define (build-frame vars vals)
-  (cons 'frame (new-frame vars vals))) 
+  (cons (cons 'frame (new-frame vars vals)) 'end)) 
 
-(define (binds frame) (cdr frame))  
+(define (binds frame) (cdar frame))  
 
 (define (frame-vars frame) (map car (binds frame)))
 
 (define (frame-vals frame) (map cdr (binds frame)))
 
+(define (curr-frame env) (car env))
+
+(define (prev-frame env) (cdr env)) 
+
 (define (add-binding-to-frame! var val frame)
   (set-cdr! frame (cons (cons var val) (binds frame)))
   'done) 
+
+(define (extend-env vars vals base-env)
+  (cons (build-frame vars vals) base-env)) 
+
+(define (lookup-variable var env)
+  (define (scan bind-list frame)
+    (cond ((equal? bind-list '())
+           (lookup-variable var (prev-frame env)))
+          ((eq? var (caar bind-list)) (cdar bind-list))
+          (else (scan (cdr bind-list) frame))))
+  (if (equal? env empty-env)
+      (error "Unbound variable" var)
+      (let* ((frame (car env))
+             (bind-list (binds frame)))
+        (scan bind-list frame)))) 
+    
+    
+
+ 
+  
+        
+        
+  
 
   
